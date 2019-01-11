a14560a27dbe2cf0aec45400be7d3c067c93e472
Alumno09 J901 <a09j901@campustls.local>
2019-01-10 20:52:09 -0500

    ejercicio ultimo

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(){
-  alert("hola");
+document.addEventListener("DOMContentLoaded", function() {
+  //alert("hola");
+  //const form = document.querySelectorAll(".form")[0];
+  //form.addEventListener("submit", function (){
+  //  alert("gracias por su compra");
+  //})
+
+  const form = document.querySelectorAll(".form")[0];
+  form.addEventListener("submit", function (){
+    const  cantidad = document.querySelectorAll(".cantidad")[0].value;
+    alert(`gracias por comprar ${cantidad} de ...`);
+  })
 });
